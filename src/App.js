82559b368d49218ca1ae6a7422ed22261423f83f
NathanPerrine <nathan.perrine@gmail.com>
2022-04-15 15:36:00 -0400

    pages added

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
 import React, { Component } from 'react'
 import { Routes, Route } from 'react-router-dom'
+import Nav from './components/Nav'
+import AlertMessage from './components/AlertMessage'
 import Home from './views/Home'
+import AllPosts from './views/AllPosts'
+import SignUp from './views/SignUp'
+import Login from './views/Login'
 
 export default class App extends Component {
 
     constructor(props){
         super(props)
         this.state = {
-            state_item: null
+            state_item: null,
+            message: null,
+            category: null,
+            loggedIn: localStorage.getItem('token') ? true : false 
         }
     }
+
+    flashMessage = (message, category) => {this.setState({message,category}) }
 
     render() {
         return (
             <> 
+                <Nav />
                 <div className="container">
+                    {this.state.message ? <AlertMessage category={this.state.category} message={this.state.message} flashMessage={this.flashMessage}/> : null}
                     <Routes>
                         <Route path="/" element={<Home />} />
+                        <Route path="/allposts" element={<AllPosts />} />
+                        <Route path="/signup" element={<SignUp />} />
+                        <Route path="/login" element={<Login />} />
                     </Routes>
                 </div>
             </>
---
+++ src/views/AllPosts.js
@@ -0,0 +1,9 @@
+import React, { useState, useEffect} from 'react'
+
+export default function AllPosts() {   
+    const [posts, setPosts] = useState([]);
+    
+    return (
+        <div>AllPosts</div>
+    )
+}
